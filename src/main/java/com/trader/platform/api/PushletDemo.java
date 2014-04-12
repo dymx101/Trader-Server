2a29b1db8ee39d5d70cd220ada182db4aa4b9070
balaamwe <balaamwe@balaamwe.(none)>
2014-04-12 16:02:21 +0800

    fwefsf

---
+++ src/main/java/com/trader/platform/api/PushletDemo.java
@@ -0,0 +1,9 @@
+package com.trader.platform.api;
+
+import java.io.Serializable;
+
+public class PushletDemo implements Serializable{
+
+	private static final long serialVersionUID = 7753669613431831941L;
+
+}
