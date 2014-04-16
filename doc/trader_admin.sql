99021808095127d967a9826d5ddc3d5207caaaaf
balaamwe <balaamwe@balaamwe.(none)>
2014-04-16 21:33:09 +0800

    8970

--- doc/trader_admin.sql
+++ doc/trader_admin.sql
@@ -5,16 +5,40 @@ CREATE TABLE `role` (
   `create_time` datetime NOT NULL COMMENT '记录建立时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
-  `id` int(11) NOT NULL COMMENT '角色描述',
+  `user_id` int(11) NOT NULL COMMENT '用户ID',
+  `role_id` int(11) NOT NULL COMMENT '角色ID',
+  `available` tinyint(1) NOT NULL DEFAULT '0' COMMENT '用户可用状态 -1-不可用, 0-待审批, 1-可用',
   `create_time` datetime NOT NULL COMMENT '记录建立时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `resource`;
+CREATE TABLE `resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '资源ID',
+  `parent_id` int(11) NOT NULL COMMENT '用户ID',
+  `privilege` tinyint(1) NOT NULL DEFAULT 0 COMMENT '权限字段 0 可见 -1 不可见 1-可操作',
+  `uri` varchar(50) NOT NULL,
+  `create_time` datetime NOT NULL COMMENT '记录建立时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `privilege`;
+CREATE TABLE `privilege` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL,
+  `create_time` datetime NOT NULL COMMENT '记录建立时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- doc/trader_main.sql
+++ doc/trader_main.sql
@@ -1,11 +1,63 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户表主键ID',
-  `nick_name` varchar(45) DEFAULT NULL COMMENT '用户昵称',
-  `gender` tinyint(1) NOT NULL DEFAULT '0' COMMENT '用户性别编码：0-未知1-男2-女',
-  `available` tinyint(1) NOT NULL DEFAULT '0' COMMENT '用户可用状态0-不可用1-可用',
-  `role_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户可用状态0-不可用1-可用',
+  `user_name` varchar(45) NOT NULL COMMENT '用户昵称',
+  `passwd` varchar(15) NOT NULL, 
+  `mail` varchar(30) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL COMMENT '绑定的手机号码',
   `create_time` datetime NOT NULL COMMENT '记录建立时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='user 数据主表';
+
+
+DROP TABLE IF EXISTS `user_account`;
+CREATE TABLE `user_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `balance` int(10) unsigned DEFAULT '0' COMMENT '金额',
+  `create_time` datetime NOT NULL COMMENT '记录建立时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `trade_flow`;
+CREATE TABLE `trade_flow` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT '0' COMMENT '交易金额：充值为正数;消费为负数',
+  `digest` varchar(50) DEFAULT NULL COMMENT '摘要',
+  `create_time` datetime NOT NULL COMMENT '记录建立时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='交易流水表';
+
+
+DROP TABLE IF EXISTS `user_relation_from`;
+CREATE TABLE `user_relation_from` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_user_id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `relationship` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1-follow -1-block',
+  `create_time` datetime NOT NULL COMMENT '记录建立时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 
+PARTITION BY HASH (from_user_id)
+PARTITIONS 256;
+
+
+DROP TABLE IF EXISTS `user_relation_to`;
+CREATE TABLE `user_relation_to` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `to_user_id` int(11) NOT NULL,
+  `from_user_id` int(11) NOT NULL,
+  `relationship` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1-follow -1-block',
+  `create_time` datetime NOT NULL COMMENT '记录建立时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据记录更新时间',
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 
+PARTITION BY HASH (to_user_id)
+PARTITIONS 256;
+
